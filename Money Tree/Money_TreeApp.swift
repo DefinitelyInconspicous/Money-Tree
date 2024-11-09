1a4b42c560a9e8f7d2af80fd68274fd925d6410e
DefinitelyInconspicous <avyan_mehra@s2023.ssts.edu.sg>
2024-11-09 14:44:50 +0800

    Initial Commit

---
+++ Money Tree/Money_TreeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Money_TreeApp.swift
+//  Money Tree
+//
+//  Created by Avyan Mehra on 9/11/24.
+//
+
+import SwiftUI
+
+@main
+struct Money_TreeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
