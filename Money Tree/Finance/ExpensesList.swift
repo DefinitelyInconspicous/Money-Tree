db00790ed320a02079d221a408399bd96c761beb
Avyan Mehra <avyan_mehra@s2023.ssts.edu.sg>
2024-11-11 13:26:58 +0800

    Add expenses

--- Money Tree/Finance/ExpensesList.swift
+++ Money Tree/Finance/ExpensesList.swift
@@ -6,13 +6,78 @@
 //
 
 import SwiftUI
+import Forever
+
+
 
 struct ExpensesList: View {
+    @Forever(wrappedValue: [Expense(amt: 0, time: .now, cat: "Sample", timeact: false), Expense(amt: 0, time: .now, cat: "Sample", timeact: false), Expense(amt: 0, time: .now, cat: "Sample", timeact: false)], "expenseList") var expenseList: [Expense]
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            VStack {
+                NavigationLink(destination: ExpensesList()) {
+                    HStack {
+                        Text("View Expenses")
+                            .font(.headline)
+                            .padding()
+                        Image(systemName: "chevron.right")
+                            .imageScale(.large)
+                            .fontWeight(.heavy)
+                    }
+                }
+                ForEach($expenseList, id: \.id) { item in
+                    var count = 3
+                    if count != 0 {
+                        Button {
+                            item.wrappedValue.timeact.toggle()
+                        } label: {
+                            VStack {
+                                HStack {
+                                    Text(item.wrappedValue.cat)
+                                        .foregroundStyle(.white)
+                                        .fontWeight(.heavy)
+                                    Spacer()
+                                    Text("\(round(item.wrappedValue.amt))")
+                                        .foregroundStyle(.white)
+                                        .fontWeight(.heavy)
+                                }
+                                if (item.wrappedValue.timeact == true) {
+                                    Text("\(item.wrappedValue.time.formatted())")
+                                        .foregroundStyle(.white)
+                                        .fontWeight(.medium)
+                                    
+                                }
+                            }
+                            .padding()
+                            .background(.green)
+                            .clipShape(RoundedRectangle(cornerRadius: 20))
+                            .padding(.horizontal)
+                        }
+                        .buttonStyle(.plain)
+                        .onAppear {
+                            count -= 1
+                        }
+                    }
+                }
+                
+                
+            }
+            .navigationTitle("Expenses")
+            .navigationBarTitleDisplayMode(.large)
+            .toolbar {
+                ToolbarItem() {
+                    Button {
+                        
+                    } label: {
+                        Image(systemName: "plus")
+                            .imageScale(.large)
+                            .fontWeight(.heavy)
+                    }
+                }
+            }
+        }
     }
 }
-
 #Preview {
     ExpensesList()
 }
