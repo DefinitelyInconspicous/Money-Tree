a35b70b2c35d4243c967379e1e4b41e9cfdadcb4
samyak <samyak_oswal@students.edu.sg>
2024-11-11 13:27:46 +0800

    Home page w/o plant

--- Money Tree/HomePage.swift
+++ Money Tree/HomePage.swift
@@ -8,8 +8,26 @@
 import SwiftUI
 
 struct HomePage: View {
+    var stars: Int = 0
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            NavigationStack {
+                HStack {
+                    Text("\(stars)")
+                        .font(.largeTitle)
+                        .offset(x: 150, y:-370)
+                        .bold()
+                    Image(systemName: "star.fill")
+                        .symbolRenderingMode(.multicolor)
+                        .font(.largeTitle)
+                        .offset(x: 150, y:-370)
+                        .shadow(color: Color("Gold"), radius: 15, y: 5)
+                        
+                }
+                .navigationTitle("Home")
+                .navigationBarTitleDisplayMode(.large)
+            }
+        }
     }
 }
 
