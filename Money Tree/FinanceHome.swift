ecc24b2df978ff731bdaa8ad4af143f2552db7f2
Avyan Mehra <avyan_mehra@s2023.ssts.edu.sg>
2024-11-11 09:31:54 +0800

    Added Finance

---
+++ Money Tree/FinanceHome.swift
@@ -0,0 +1,29 @@
+//
+//  FinanceHome.swift
+//  Money Tree
+//
+//  Created by Avyan Mehra on 11/11/24.
+//
+
+import SwiftUI
+
+struct FinanceHome: View {
+    var body: some View {
+        NavigationStack {
+            VStack {
+                VStack {
+                    Text("View Expenses")
+                        .font(.headline)
+                    NavigationLink(destination: ExpensesList()) {
+                        Text("")
+                    }
+                }
+            }
+            .navigationTitle("Finance")
+        }
+    }
+}
+
+#Preview {
+    FinanceHome()
+}
